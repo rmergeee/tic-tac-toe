67e1d50879d71f3fe600360b83cec4805c7d1d75
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-08-16 16:29:56 +0300

    finish CosoleUI

--- src/logic/gamecontroller.js
+++ src/logic/gamecontroller.js
@@ -76,6 +76,7 @@ const GameController = function (
         checkTie,
         getGameStatus,
         changeGameStatus,
+        switchPlayerTurn,
     };
 };
 
--- src/ui/consoleUI.js
+++ src/ui/consoleUI.js
@@ -1,6 +1,7 @@
 import GameController from "../logic/gamecontroller.js";
+import Gameboard from "../logic/gameboard.js";
 import PromptSync from "prompt-sync";
-const prompt = PromptSync();  
+const prompt = PromptSync();
 
 const log = console.log;
 
@@ -8,13 +9,42 @@ const ConsoleUI = (function () {
 
     const firstPlayerName = prompt("Введіть ім'я гравця X: ");
     const secondPlayerName = prompt("Введіть ім'я гравця O: ");
-    log("Хід гравця - " + firstPlayerName);
     const contrl = GameController(firstPlayerName, secondPlayerName);
 
     const playGame = () => {
         while (contrl.getGameStatus() === false) {
-            log("Хід гравця - " + firstPlayerName)
-            contrl.changeGameStatus();
+            log("Хід гравця - " + contrl.getActivePlayer().name);
+
+            let row;
+            let column;
+
+            do {
+                do {
+                    row = Number(prompt("Введіть рядок (0-2): "));
+                } while (isNaN(row) || row < 0 || row > 2);
+
+                do {
+                    column = Number(prompt("Введіть колонку (0-2): "));
+                } while (isNaN(column) || column < 0 || column > 2);
+
+                if(Gameboard.getBoard()[row][column] !== null) log("Ця клітинка зайнята! Оберіть іншу.");
+
+            } while (Gameboard.getBoard()[row][column] !== null)
+
+            Gameboard.placeToken(row, column, contrl.getActivePlayer().token);
+            console.table(Gameboard.getBoard());
+
+            if (contrl.checkWin(Gameboard.getBoard(), contrl.getActivePlayer().token)) {
+                log(contrl.getActivePlayer().name + " win!");
+                contrl.changeGameStatus();
+            }
+
+            if (contrl.checkTie(Gameboard.getBoard())) {
+                log("Tie!");
+                contrl.changeGameStatus();
+            }
+
+            contrl.switchPlayerTurn();
         }
     }
 
