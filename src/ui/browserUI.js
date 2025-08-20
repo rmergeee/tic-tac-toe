5b2561781fa9e64cbaceb0b3a7fa18c2b5961da8
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-08-20 17:27:33 +0300

    add choice players name

--- src/ui/browserUI.js
+++ src/ui/browserUI.js
@@ -1,11 +1,23 @@
+import GameController from "../logic/gamecontroller.js";
+import Gameboard from "../logic/gameboard.js";
+
 const dialog = document.getElementById('dialog');
+const dialogForm = document.querySelector(".dialog-form");
+const player1NameOnPage = document.querySelector(".player1");
+const player2NameOnPage = document.querySelector(".player2");
+const board = document.querySelector(".board");
 
-function openDialog() {
-  dialog.showModal(); // Відкриває як модальне вікно (блокує фон)
-}
+dialog.showModal();
 
-function closeDialog() {
-  dialog.close(); // Закриває вікно
-}
+let playerNames = {};
 
-openDialog();
+dialogForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(dialogForm)
+    formData.forEach((value, key) => playerNames[key] = value);
+
+    if (playerNames.p1name !== "") player1NameOnPage.textContent = playerNames.p1name;
+    if (playerNames.p2name !== "") player2NameOnPage.textContent = playerNames.p2name;
+
+    dialog.close();
+})
